e04022e2661e047989e2dd4b687f8119da07a8bd
Ayush Gupta <ayush@AyushMacPro.ht.home>
2025-06-20 09:24:52 -0400

    setup better autocomplete

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,8 +1,8 @@
 -- Copy to system clipboard in normal and visual mode
 vim.keymap.set({'n','v'}, '<C-c>', 'y', { noremap = true })
 
--- Cut to system clipboard in normal and visual mode
-vim.keymap.set({'n','v'}, '<C-x>', 'd', { noremap = true })
+-- Copy to system clipboard in normal and visual mode
+vim.keymap.set({ 'n', 'i', 'v', 'x' }, '<C-c>', '"+y', { noremap = true, desc = 'MacOS copy' })
 
 -- Paste from system clipboard
 vim.keymap.set({'n','v'}, '<C-v>', 'p', { noremap = true })
@@ -14,6 +14,36 @@ vim.keymap.set({'n','i'}, '<C-z>', '<esc>ui', { noremap = true })
 -- redo
 vim.keymap.set({'n','i'}, '<C-y>', '<esc><C-r>i', { noremap = true })
 
+
+----------------------------------------------------------------------
+--MacOS specific binds
+----------------------------------------------------------------------
+
+--saving
+vim.keymap.set({'n','i'}, '<D-s>', '<esc>:w<cr>i', { noremap = true })
+-- undo
+vim.keymap.set({'n','i'}, '<D-z>', '<esc>ui', { noremap = true })
+-- redo
+vim.keymap.set({'n','i'}, '<D-y>', '<esc><C-r>i', { noremap = true })
+-- Copy to system clipboard in normal and visual mode
+vim.keymap.set({ 'n', 'i', 'v', 'x' }, '<D-c>', '"+y', { noremap = true, desc = 'MacOS copy' })
+-- Cut to system clipboard in normal and visual mode
+vim.keymap.set({'n','v'}, '<D-x>', 'd', { noremap = true })
+
+
+----------------------------------------------------------------------
+-- navigation keymaps
+----------------------------------------------------------------------
+vim.keymap.set({'n', 'i'}, "<A-h>", "<C-w>h")
+vim.keymap.set({'n', 'i'}, "<A-j>", "<C-w>j")
+vim.keymap.set({'n', 'i'}, "<A-k>", "<C-w>k", {noremap = true})
+vim.keymap.set({'n', 'i'}, "<A-l>", "<C-w>l")
+
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope.find_files, {desc = "find files"})
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, {desc = "grep"})
+vim.keymap.set('n', '<leader>fb', telescope.buffers, {desc = "buffers"})
+vim.keymap.set('n', '<leader>fh', telescope.help_tags, {desc = "help_tags"})
 
 ----------------------------------------------------------------------
 -- harpoon keymaps
@@ -36,5 +66,5 @@ vim.keymap.set("n", "<A-8>", function() harpoon:list():select(8) end)
 vim.keymap.set("n", "<A-9>", function() harpoon:list():select(9) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<A-p>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<A-n>", function() harpoon:list():next() end)
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,15 @@
 return {
-        "mason-org/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
         opts = {},
         dependencies = {
             { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
+            'saghen/blink.cmp'
         },
+        config = function() 
+                local capabilitites = require('blink.cmp').get_lsp_capabilities()
+                require("lspconfig").lua_ls.setup {capabilitites = capabilitites}
+                require("lspconfig").gopls.setup {capabilitites = capabilitites}
+                require("lspconfig").zls.setup {capabilitites = capabilitites}
+                require("lspconfig").rust_analyzer.setup {capabilitites = capabilitites}
+        end,
     }
